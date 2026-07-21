750ed13024f8fcc0f54b11a9a5890b855a1b0616
Michael Eastwood <michael.w.eastwood@gmail.com>
2026-07-20 19:09:15 -0700

    fix: configure linux platform to map web FirebaseOptions in firebase_options_dev.dart/firebase_options_prod.dart

--- app/lib/main_prod.dart
+++ app/lib/main_prod.dart
@@ -3,17 +3,12 @@ import 'package:flutter/material.dart';
 import 'firebase_options_prod.dart';
 import 'main.dart';
 
-import 'package:flutter/foundation.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (defaultTargetPlatform != TargetPlatform.linux &&
-      defaultTargetPlatform != TargetPlatform.windows) {
-    if (Firebase.apps.isEmpty) {
-      await Firebase.initializeApp(
-        options: DefaultFirebaseOptions.currentPlatform,
-      );
-    }
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
   }
   AppConfig.environment = AppEnvironment.prod;
   mainCommon();
